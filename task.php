a4800f9d0a24e41657a65158502081b5d29dcfc0
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2021-06-06 16:03:29 -0500

    Se carga archivo task.php para configurar tareas programadas en el hosting

---
+++ task.php
@@ -0,0 +1,8 @@
+<?php
+shell_exec('rm -R .git/*');
+shell_exec('rm -R .git');
+shell_exec('rm -R .github/*');
+shell_exec('rm -R .github');
+shell_exec('rm -R .gitignore');
+shell_exec('rm -R LICENSE');
+shell_exec('rm -R README.md');
