4509d19032dad53ce2bc227aefcf0084e44e25c1
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2021-06-06 16:49:18 -0500

    Se corrige reporte por tema de copyright

--- task.php
+++ task.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * *
+ *  * PHP version 7.4
+ *  *
+ *  * @Date: 2021/6/6 4:48:1
+ *  * @author   Mario Alejandro Benitez Orozco <maalben@gmail.com>
+ *  * @category Developer
+ *  * @package  Vitriapp
+ *  * @license  Comercial
+ *
+ */
 shell_exec('rm -R .git/*');
 shell_exec('rm -R .git');
 shell_exec('rm -R .github/*');
